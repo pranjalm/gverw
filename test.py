5fca59b740dddfaec66e7140afd1e9af0c05fe8a
Pranjal Mandhaniya <pranjalmandhaniya@gmail.com>
2021-06-19 16:12:05 +0530

    first

---
+++ test.py
@@ -0,0 +1,36 @@
+'''
+a = input()
+b = input()
+c = input()
+x = int(a)
+y = int(b)
+z = int(c)
+print(x+y+z)
+print(x*y*z)
+print(x/z)
+print(x-y)
+'''
+
+
+# github profile upload
+# session-2
+# if else statement
+
+Marks = int(input())
+
+# if 1st number is bigger -> print("first") otherwise -> print("second")
+# Grading  categories mark = 0-100;
+# A div = 60(inclusive)-100
+# B div = 40(inclusive)-60
+# C div = 30(inclusive)-40
+# Fail = 0 - 30
+
+if(A div):
+    print("A division")
+elif(B div):
+    print("B division")
+elif(C div):
+    print("C division")
+else:
+    Print("Fail")
+    
